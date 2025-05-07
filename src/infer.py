41e51c9aa7b28af315745608cb6772615f177532
Bartosz Stachowiak <sbartekt@op.pl>
2025-05-07 19:38:20 +0200

    reimplement inference script

--- src/infer.py
+++ src/infer.py
@@ -1,12 +1,18 @@
+from __future__ import annotations
+
+import argparse
+import dataclasses
+import json
 import logging
+import pathlib
 
 import cv2
 import numpy as np
+import pytorch_lightning as pl
 import torch
 import torchvision.transforms as T
 import tqdm
 
-from src import const
 from src.config import Config
 from src.data import dataset, transforms
 
@@ -19,23 +25,89 @@ logger = logging.getLogger(__name__)
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
+@dataclasses.dataclass
+class InferArgs:
+    config: Config
+    weigths_path: pathlib.Path
+    input_dir: pathlib.Path
+    output_dir: pathlib.Path
+
+    def __post_init__(self):
+        if isinstance(self.config, pathlib.Path):
+            data = json.loads(self.config.read_text())
+            self.config = Config(**data)
+        assert isinstance(self.config, Config), "Config must be a Config object"
+        assert self.weigths_path.exists(), "Weights path does not exist"
+        assert self.input_dir.exists(), "Input directory does not exist"
+        self.output_dir.mkdir(parents=True, exist_ok=True)
+
+    @classmethod
+    def from_args(cls) -> InferArgs:
+        parser = argparse.ArgumentParser(
+            description="Inference script for background removal"
+        )
+        parser.add_argument(
+            "--config",
+            type=pathlib.Path,
+            required=True,
+            help="Path to the config file",
+        )
+        parser.add_argument(
+            "--weights-path",
+            type=pathlib.Path,
+            required=True,
+            help="Path to the model weights file",
+        )
+        parser.add_argument(
+            "--input-dir",
+            type=pathlib.Path,
+            required=True,
+            help="Path to the input directory containing input frames",
+        )
+        parser.add_argument(
+            "--output-dir",
+            type=pathlib.Path,
+            required=True,
+            help="Path to the output directory to save results",
+        )
+
+        return cls(**vars(parser.parse_args()))
+
+    def get_model(self) -> pl.LightningModule:
+        model = self.config.get_model()
+        model.load_state_dict(
+            torch.load(self.weigths_path, map_location=DEVICE)["state_dict"]
+        )
+        return model
+
+
+def tensor_to_numpy_img(tensor: torch.Tensor) -> np.ndarray:
+    if tensor.ndim == 3:
+        tensor = tensor.unsqueeze(0)
+    assert tensor.ndim == 4
+    tensor = tensor.permute(0, 2, 3, 1)  # (B, H, W, C)
+    np_arr = (tensor.cpu().numpy() * 255.0).astype(np.uint8)
+    return np_arr
+
+
 if __name__ == "__main__":
-    config = Config.from_args()
-    config.prepare_inference()
-    model = config.get_model().to(DEVICE)
-    img_paths = sorted(config.data_dir.glob("**/*.npy"))
-    logger.info(f"Found {len(img_paths)} images in {config.data_dir}")
-    infer_dataset = dataset.BGREMDataset(
+    infer_args = InferArgs.from_args()
+    model = infer_args.get_model().to(DEVICE)
+
+    img_paths = sorted(infer_args.input_dir.glob("**/*.npy"))
+    logger.info(f"Found {len(img_paths)} images in {infer_args.input_dir}")
+    infer_dataset = dataset.BGREADataset(
         img_paths,
-        masker=transforms.DiffIntensityMasker(config.diff_intensity),
-        bgr_transform=T.Compose([T.ToTensor()]),
-        separate_event_channel=config.event_channel,
-        yuv_interpolation=config.yuv_interpolation,
-        blur_factor=config.mask_blur_factor,
+        transform=T.Compose(
+            [
+                transforms.EventMaskChannelRemover(),
+                T.ToTensor(),
+            ]
+        ),
     )
     infer_loader = torch.utils.data.DataLoader(
         infer_dataset,
-        batch_size=config.batch_size,
+        batch_size=infer_args.config.batch_size,
         num_workers=4,
         shuffle=False,
         persistent_workers=True,
@@ -43,32 +115,29 @@ if __name__ == "__main__":
     num_batches = len(infer_loader)
     logger.info(f"Starting inference on {num_batches} batches")
     iter_batches = tqdm.tqdm(infer_loader, total=num_batches)
-    all_bgrem = []
-    for batch in iter_batches:
-        bgrem, target = batch
-        bgr_orig = bgrem[:, :3].detach().cpu().numpy()
-        bgr_orig = np.transpose(bgr_orig, (0, 2, 3, 1))
-        last_channel = bgrem.shape[1] - 1
-        mask = bgrem[:, last_channel].unsqueeze(-1).detach().cpu().numpy()
-        bgrem = bgrem.to(DEVICE)
-        bgr = model(bgrem)
-        bgr = bgr.detach().cpu().numpy()
+    all_estimate_maps = []
+    all_rec_frames = []
+    for x, y in iter_batches:
+        est_map, rec_frames = model(x.to(DEVICE))
+        all_estimate_maps.extend(tensor_to_numpy_img(est_map))
+        all_rec_frames.extend(tensor_to_numpy_img(rec_frames))
 
-        bgr = np.transpose(bgr, (0, 2, 3, 1))
-        if not config.full_pred:
-            bgr = np.where(mask, bgr, bgr_orig)
-        bgr = np.clip(bgr * 255, 0, 255).astype(np.uint8)
-        all_bgrem.append(bgr)
-
-    all_bgrem = np.concatenate(all_bgrem, axis=0)
-
-    logger.info(f"Saving output to {config.output}")
+    map_out = infer_args.output_dir / "estimate_map.mp4"
+    rec_out = infer_args.output_dir / "reconstructed.mp4"
+    logger.info(f"Saving estimate map to {map_out}")
     out = cv2.VideoWriter(
-        str(config.output),
-        cv2.VideoWriter_fourcc(*"mp4v"),
+        str(map_out),
+        cv2.VideoWriter_fourcc(*"mp4v"),  # type: ignore
         30,
-        (const.IMG_WIDTH, const.IMG_HEIGHT),
+        (all_estimate_maps[0].shape[1], all_estimate_maps[0].shape[0]),
     )
-    for frame in all_bgrem:
-        out.write(frame)
+    for img in all_estimate_maps:
+        out.write(img)
     out.release()
+    logger.info(f"Saving reconstructed frames to {rec_out}")
+    out = cv2.VideoWriter(
+        str(rec_out),
+        cv2.VideoWriter_fourcc(*"mp4v"),  # type: ignore
+        30,
+        (all_rec_frames[0].shape[1], all_rec_frames[0].shape[0]),
+    )

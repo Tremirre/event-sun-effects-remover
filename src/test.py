f90a46b13b51ca33a5cfd540ce66d8fa28a085f9
Stachowiak, Bartosz <bartosz.stachowiak@aptiv.com>
2025-06-13 20:15:40 +0200

    fix bugs with missing sigmoid casting on the detector output

--- src/test.py
+++ src/test.py
@@ -211,7 +211,7 @@ def main():
         bgr_img_tensor = T.ToTensor()(bgr_img).unsqueeze(0).to(DEVICE)
         mask_tensor = T.ToTensor()(mask).unsqueeze(0)
         with torch.no_grad():
-            est_map = model.detector(bgr_img_tensor).cpu()
+            est_map = F.sigmoid(model.detector(bgr_img_tensor)).cpu()
 
         for threshold in THRESHOLDS:
             preds = est_map > threshold
@@ -239,7 +239,7 @@ def main():
         bgr_img_tensor = T.ToTensor()(bgr_img).unsqueeze(0).to(DEVICE)
         mask_tensor = T.ToTensor()(mask).unsqueeze(0)
         with torch.no_grad():
-            est_map = model.detector(bgr_img_tensor[:, :3])
+            est_map = F.sigmoid(model.detector(bgr_img_tensor[:, :3]))
             if combiner_detection:
                 est_map = model.combiner(bgr_img_tensor, est_map)[:, 4]
             est_map = est_map.cpu()
